9e2c13d14ee4e3b233212cbe8ed1e145ff545148
amirmojarad <amirmojarad13@gmail.com>
2021-08-05 11:04:17 +0430

    tests complete

--- server/app/test/test_sql_app.py
+++ server/app/test/test_sql_app.py
@@ -41,8 +41,48 @@ def test_get_coffee_image():
     response = client.get(f"/coffee/{coffee_name}/image/", )
     assert response.status_code == 200
 
+
 # User Section Test
+def test_user_sign_up():
+    username = "test2"
+    response = client.post("/users/sign_up", data={"username": username, "password": "test_password"})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["email"] is None
+    assert data["username"] == username
+    assert len(data["access_token"]) == 124
 
 
+def test_get_user():
+    username = "new_test"
+    make_user_response = client.post("/users/sign_up", data={"username": username, "password": "test_password"})
+    assert make_user_response.status_code == 200
+    access_token = make_user_response.json()["access_token"]
+    response = client.get(f"/users/{username}", headers={"token": access_token})
+    assert response.status_code == 200
 
 
+def test_put_user():
+    username = "new_test2"
+    full_name = "full_name"
+    email = "email"
+    make_user_response = client.post("/users/sign_up", data={"username": username, "password": "test_password"})
+    assert make_user_response.status_code == 200
+    access_token = make_user_response.json()["access_token"]
+    response = client.put(f"/users/{username}", headers={"token": access_token},
+                          params={"full_name": full_name, "email": email})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["full_name"] == full_name
+    assert data["email"] == email
+
+
+def test_post_coffee_to_user():
+    username = "new_test6"
+    sign_up_response = client.post("/users/sign_up", data={"username": username, "password": "32"})
+    assert sign_up_response.status_code == 200
+    access_token = sign_up_response.json()["access_token"]
+    coffee_name = "Americano"
+    response = client.post(f"/users/{username}/coffee/", params={"coffee_name": coffee_name},
+                           headers={"token": access_token})
+    assert response.status_code == 200
